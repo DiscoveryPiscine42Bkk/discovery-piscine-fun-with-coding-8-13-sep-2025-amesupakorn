428e76056b607e9d2c7c4d952752413519aae3a7
SupakornThongaerd <supakorn642@gmail.com>
2025-09-12 14:01:55 +0700

    refactor

--- cell03/ex00/backgroound.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('change-bg');
-btn.addEventListener('click', () => {
-  const randomColor = `rgb(
-    ${Math.floor(Math.random() * 256)},
-    ${Math.floor(Math.random() * 256)},
-    ${Math.floor(Math.random() * 256)}
-  )`;
-  document.body.style.backgroundColor = randomColor;
-});
